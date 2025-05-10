598344be040b06c51bf54a488d923e2aa5338000
ZarkerZ <jamesgammer14@gmail.com>
2025-05-10 13:10:47 +0800

    James Commitkkkkk

---
+++ app/src/main/java/com/intprog32/caterspot30/PendingOrder.kt
@@ -0,0 +1,16 @@
+package com.intprog32.caterspot30
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PendingOrder : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pending_order)
+
+    }
+}
