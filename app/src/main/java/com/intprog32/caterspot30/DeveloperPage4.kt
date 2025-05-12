a09774c27ce9ab7bb454086e868a65d72589d602
rianel <zaxford225@gmail.com>
2025-05-13 00:34:49 +0800

    new

--- app/src/main/java/com/intprog32/caterspot30/DeveloperPage4.kt
+++ app/src/main/java/com/intprog32/caterspot30/DeveloperPage4.kt
@@ -3,12 +3,8 @@ package com.intprog32.caterspot30
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.net.Uri
 
 class DeveloperPage4 : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +12,23 @@ class DeveloperPage4 : Activity() {
         setContentView(R.layout.activity_developer_page4)
 
         val backButton = findViewById<ImageView>(R.id.backButton)
-        backButton.setOnClickListener { view: View? ->
-            startActivity(
-                Intent(
-                    this@DeveloperPage4,
-                    DeveloperPage::class.java
-                )
-            )
+        backButton.setOnClickListener {
+            startActivity(Intent(this@DeveloperPage4, DeveloperPage::class.java))
             finish()
+        }
+
+        val facebookIcon = findViewById<ImageView>(R.id.facebookIcon)
+
+        facebookIcon.setOnClickListener {
+            openUrl("https://www.facebook.com/rianel.lumbaca.3")
+        }
+    }
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
         }
     }
 }
